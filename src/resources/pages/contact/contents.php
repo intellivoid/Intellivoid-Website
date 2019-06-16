5446956fbd9ef9c08200206729e0f77e1499785c
Narrakas <netkas@users.noreply.github.com>
2019-06-15 20:29:58 -0400

    Designed /contact page

--- src/resources/pages/contact/contents.php
+++ src/resources/pages/contact/contents.php
@@ -14,61 +14,81 @@
     <body class="index-page">
         <?PHP HTML::importSection('landing_navbar'); ?>
         <div class="wrapper">
+            <div class="page-header">
+                <div class="page-header-image"></div>
 
-            <section class="section">
-                <div class="container">
-                    <div class="row justify-content-between">
-                        <div class="col-md-6">
-                            <div class="card card-plain">
-                                <div class="card-header">
-                                    <h1 class="profile-title text-left">Contact</h1>
+                <div class="content">
+                    <div class="container">
+
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="card card-plain">
+                                    <div class="card-header">
+                                        <h1 class="profile-title text-left">Contact Us</h1>
+                                    </div>
+                                    <div class="card-body">
+                                        <form>
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label class="text-primary" for="email">
+                                                            <i class="fa fa-envelope"></i> Email address
+                                                        </label>
+                                                        <input name="email" id="email" type="email" class="form-control" placeholder="affoe_x_node@email.com">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label class="text-primary" for="subject">
+                                                            <i class="fa fa-question"></i> Subject
+                                                        </label>
+                                                        <input name="subject" id="subject" type="text" class="form-control" placeholder="Subject">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="form-group">
+                                                        <label class="text-primary" for="message">
+                                                            <i class="fa fa-pencil-alt"></i> Message
+                                                        </label>
+                                                        <textarea name="message" id="message" type="text" rows="5" class="form-control"></textarea>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <input type="submit" class="btn btn-primary btn-round float-right" value="Submit">
+                                        </form>
+                                    </div>
                                 </div>
-                                <div class="card-body">
-                                    <form>
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label>Your Name</label>
-                                                    <input type="text" class="form-control" value="Mike">
-                                                </div>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label>Email address</label>
-                                                    <input type="email" class="form-control" placeholder="mike@email.com">
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label>Phone</label>
-                                                    <input type="text" class="form-control" value="001-12321345">
-                                                </div>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label>Company</label>
-                                                    <input type="text" class="form-control" value="CreativeTim">
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-md-12">
-                                                <div class="form-group">
-                                                    <label>Message</label>
-                                                    <input type="text" class="form-control" placeholder="Hello there!">
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <button type="submit" class="btn btn-primary btn-round float-right" rel="tooltip" data-original-title="Can't wait for your message" data-placement="right">Send text</button>
-                                    </form>
+                            </div>
+                            <div class="col-md-4 ml-auto">
+                                <div class="info info-horizontal pt-1">
+                                    <div class="icon icon-primary">
+                                        <i class="fa fa-bolt"></i>
+                                    </div>
+                                    <div class="description">
+                                        <h4 class="info-title">We will get back to you!</h4>
+                                        <p>
+                                            Contacting us is fast and painless, no unnecessary steps or information.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="info info-horizontal">
+                                    <div class="icon icon-primary">
+                                        <i class="fa fa-question"></i>
+                                    </div>
+                                    <div class="description">
+                                        <h4 class="info-title">Have a question?</h4>
+                                        <p>We will answer them! No generic or confusing responses. We don't use any AI behind our support team</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+
                     </div>
                 </div>
-            </section>
+
+            </div>
             <?PHP HTML::importSection('landing_footer'); ?>
         </div>
         <?PHP HTML::importSection('landing_javascript'); ?>
