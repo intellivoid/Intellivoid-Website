2b58fa38bca5f0717b3d9405ec45d109d3e91762
netkas <netkas@intellivoid.info>
2020-02-16 16:31:12 -0300

    Updated landing page

--- src/resources/sections/landing_footer.php
+++ src/resources/sections/landing_footer.php
@@ -1,3 +1,6 @@
+<?PHP
+    use DynamicalWeb\HTML;
+?>
 <footer class="footer">
     <div class="container">
         <div class="row">
@@ -7,13 +10,13 @@
             <div class="col-md-3">
                 <ul class="nav">
                     <li class="nav-item">
-                        <a href="/contact" class="nav-link"><?PHP \DynamicalWeb\HTML::print(TEXT_FOOTER_CONTACT_US); ?></a>
+                        <a href="/contact" class="nav-link"><?PHP HTML::print(TEXT_FOOTER_CONTACT_US); ?></a>
                     </li>
                     <li class="nav-item">
-                        <a href="/privacy" class="nav-link"><?PHP \DynamicalWeb\HTML::print(TEXT_FOOTER_PRIVACY_POLICY); ?></a>
+                        <a href="/privacy" class="nav-link"><?PHP HTML::print(TEXT_FOOTER_PRIVACY_POLICY); ?></a>
                     </li>
                     <li class="nav-item">
-                        <a href="/tos" class="nav-link"><?PHP \DynamicalWeb\HTML::print(TEXT_FOOTER_TERMS_OF_SERVICE); ?></a>
+                        <a href="/tos" class="nav-link"><?PHP HTML::print(TEXT_FOOTER_TERMS_OF_SERVICE); ?></a>
                     </li>
                 </ul>
             </div>
@@ -23,15 +26,18 @@
                         <a href="https://openblu.intellivoid.info/" class="nav-link">OpenBlu VPN</a>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link">Netlenium <?PHP \DynamicalWeb\HTML::print(TEXT_FOOTER_COMING_SOON_PLACEHOLDER); ?></a>
+                        <a href="https://accounts.intellivoid.info" class="nav-link">Accounts</a>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link">Accounts <?PHP \DynamicalWeb\HTML::print(TEXT_FOOTER_COMING_SOON_PLACEHOLDER); ?></a>
+                        <a href="https://coffeehouse.intellivoid.info" class="nav-link">CoffeeHouse</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Netlenium <?PHP HTML::print(TEXT_FOOTER_COMING_SOON_PLACEHOLDER); ?></a>
                     </li>
                 </ul>
             </div>
             <div class="col-md-3">
-                <h3 class="title"><?PHP \DynamicalWeb\HTML::print(TEXT_FOOTER_FOLLOW_US_TITLE); ?></h3>
+                <h3 class="title"><?PHP HTML::print(TEXT_FOOTER_FOLLOW_US_TITLE); ?></h3>
                 <div class="btn-wrapper profile">
                     <button onclick="window.open('https://twitter.com/Intellivoid');" class="btn btn-icon btn-simple btn-round btn-neutral">
                         <i class="fab fa-twitter"></i>
--- src/resources/sections/landing_navbar.php
+++ src/resources/sections/landing_navbar.php
@@ -1,10 +1,15 @@
+<?PHP
+
+use DynamicalWeb\DynamicalWeb;
+use DynamicalWeb\HTML;
+?>
 <nav class="navbar navbar-expand-lg fixed-top navbar-transparent" color-on-scroll="100">
     <div class="container">
         <div class="navbar-translate">
-            <a class="navbar-brand" href="/" rel="tooltip" title="<?PHP \DynamicalWeb\HTML::print(TEXT_NAVBAR_BRAND_TOOLTIP); ?>" data-placement="bottom">
+            <a class="navbar-brand" href="<?PHP DynamicalWeb::getRoute('index', array(), true); ?>" rel="tooltip" title="<?PHP HTML::print(TEXT_NAVBAR_BRAND_TOOLTIP); ?>" data-placement="bottom">
                 <span>INTELLIVOID•</span> Technologies
             </a>
-            <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="<?PHP \DynamicalWeb\HTML::print(TEXT_NAVBAR_TOGGLE_LABEL); ?>">
+            <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="<?PHP HTML::print(TEXT_NAVBAR_TOGGLE_LABEL); ?>">
                 <span class="navbar-toggler-bar bar1"></span>
                 <span class="navbar-toggler-bar bar2"></span>
                 <span class="navbar-toggler-bar bar3"></span>
@@ -17,7 +22,7 @@
                         <a>INTELLIVOID•</a>
                     </div>
                     <div class="col-6 collapse-close text-right">
-                        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="<?PHP \DynamicalWeb\HTML::print(TEXT_NAVBAR_TOGGLE_LABEL); ?>">
+                        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="<?PHP HTML::print(TEXT_NAVBAR_TOGGLE_LABEL); ?>">
                             <i class="tim-icons icon-simple-remove"></i>
                         </button>
                     </div>
@@ -25,25 +30,25 @@
             </div>
             <ul class="navbar-nav">
                 <li class="nav-item p-0">
-                    <a class="nav-link" rel="tooltip" title="<?PHP \DynamicalWeb\HTML::print(TEXT_NAVBAR_SOCIAL_TWITTER_TOOLTIP); ?>" data-placement="bottom" href="https://twitter.com/Intellivoid" target="_blank">
+                    <a class="nav-link" rel="tooltip" title="<?PHP HTML::print(TEXT_NAVBAR_SOCIAL_TWITTER_TOOLTIP); ?>" data-placement="bottom" href="https://twitter.com/Intellivoid" target="_blank">
                         <i class="fab fa-twitter"></i>
                         <p class="d-lg-none d-xl-none">Twitter</p>
                     </a>
                 </li>
                 <li class="nav-item p-0">
-                    <a class="nav-link" rel="tooltip" title="<?PHP \DynamicalWeb\HTML::print(TEXT_NAVBAR_SOCIAL_TELEGRAM_TOOLTIP); ?>" data-placement="bottom" href="https://t.me/Intellivoid" target="_blank">
+                    <a class="nav-link" rel="tooltip" title="<?PHP HTML::print(TEXT_NAVBAR_SOCIAL_TELEGRAM_TOOLTIP); ?>" data-placement="bottom" href="https://t.me/Intellivoid" target="_blank">
                         <i class="fab fa-telegram-plane"></i>
                         <p class="d-lg-none d-xl-none">Telegram</p>
                     </a>
                 </li>
                 <li class="nav-item p-0">
-                    <a class="nav-link" rel="tooltip" title="<?PHP \DynamicalWeb\HTML::print(TEXT_NAVBAR_SOCIAL_INSTAGRAM_TOOLTIP); ?>" data-placement="bottom" href="https://instagram.com/Intellivoid" target="_blank">
+                    <a class="nav-link" rel="tooltip" title="<?PHP HTML::print(TEXT_NAVBAR_SOCIAL_INSTAGRAM_TOOLTIP); ?>" data-placement="bottom" href="https://instagram.com/Intellivoid" target="_blank">
                         <i class="fab fa-instagram"></i>
                         <p class="d-lg-none d-xl-none">Instagram</p>
                     </a>
                 </li>
                 <li class="nav-item p-0">
-                    <a class="nav-link" rel="tooltip" title="<?PHP \DynamicalWeb\HTML::print(TEXT_NAVBAR_SOCIAL_GITHUB_TOOLTIP); ?>" data-placement="bottom" href="https://github.com/Intellivoid" target="_blank">
+                    <a class="nav-link" rel="tooltip" title="<?PHP HTML::print(TEXT_NAVBAR_SOCIAL_GITHUB_TOOLTIP); ?>" data-placement="bottom" href="https://github.com/Intellivoid" target="_blank">
                         <i class="fab fa-github"></i>
                         <p class="d-lg-none d-xl-none">Github</p>
                     </a>
@@ -51,22 +56,22 @@
 
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" id="services-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <p><?PHP \DynamicalWeb\HTML::print(TEXT_NAVBAR_SERVICES_DROPDOWN); ?></p>
+                        <p><?PHP HTML::print(TEXT_NAVBAR_SERVICES_DROPDOWN); ?></p>
                     </a>
                     <div class="dropdown-menu" aria-labelledby="services-dropdown">
                         <a class="dropdown-item" href="https://openblu.intellivoid.info/">OpenBlu</a>
-                        <a class="dropdown-item" href="#">Netlenium <?PHP \DynamicalWeb\HTML::print(TEXT_NAVBAR_COMING_SOON_PLACEHOLDER); ?></a>
-                        <a class="dropdown-item" href="#">Accounts <?PHP \DynamicalWeb\HTML::print(TEXT_NAVBAR_COMING_SOON_PLACEHOLDER); ?></a>
+                        <a class="dropdown-item" href="#">Netlenium <?PHP HTML::print(TEXT_NAVBAR_COMING_SOON_PLACEHOLDER); ?></a>
+                        <a class="dropdown-item" href="#">Accounts <?PHP HTML::print(TEXT_NAVBAR_COMING_SOON_PLACEHOLDER); ?></a>
                     </div>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" id="services-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <p><?PHP \DynamicalWeb\HTML::print(TEXT_NAVBAR_SUPPORT_DROPDOWN); ?></p>
+                        <p><?PHP HTML::print(TEXT_NAVBAR_SUPPORT_DROPDOWN); ?></p>
                     </a>
                     <div class="dropdown-menu" aria-labelledby="services-dropdown">
-                        <a class="dropdown-item" href="/contact"><?PHP \DynamicalWeb\HTML::print(TEXT_NAVBAR_SUPPORT_CONTACT_US); ?></a>
-                        <a class="dropdown-item" href="/privacy"><?PHP \DynamicalWeb\HTML::print(TEXT_NAVBAR_SUPPORT_PRIVACY_POLICY); ?></a>
-                        <a class="dropdown-item" href="/tos"><?PHP \DynamicalWeb\HTML::print(TEXT_NAVBAR_SUPPORT_TERMS_OF_SERVICE); ?></a>
+                        <a class="dropdown-item" href="/contact"><?PHP HTML::print(TEXT_NAVBAR_SUPPORT_CONTACT_US); ?></a>
+                        <a class="dropdown-item" href="/privacy"><?PHP HTML::print(TEXT_NAVBAR_SUPPORT_PRIVACY_POLICY); ?></a>
+                        <a class="dropdown-item" href="/tos"><?PHP HTML::print(TEXT_NAVBAR_SUPPORT_TERMS_OF_SERVICE); ?></a>
                     </div>
                 </li>
                 <li class="nav-item dropdown">
